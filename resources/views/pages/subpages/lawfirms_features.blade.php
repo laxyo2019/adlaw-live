6c19e272923084606aa61c6467bfbd136e099a59
ritesh panchal <riteshpanchal@laxyo.in>
2019-12-13 16:46:25 +0530

    Search lawfirms page

--- resources/views/pages/subpages/lawfirms_features.blade.php
+++ resources/views/pages/subpages/lawfirms_features.blade.php
@@ -1,10 +1,10 @@
 @extends("layouts.default")
 @section('content')
-<div class="container mt-5">
-	<div class="row">
-		<div class="col-md-12">
-			
-		</div>
+<div class="container py-4">
+    <div class="row border-bottom">
+    	<div class="col-sm-12 col-md-12 col-xl-12 text-center mb-2">
+            <h2 class="h1-responsive font-weight-bold text-center my-4 text-uppercase">Lawyer / LawFirms</h2>          
+        </div>
 	</div>
 </div>
 @endsection
--- resources/views/pages/why_adlaw.blade.php
+++ resources/views/pages/why_adlaw.blade.php
@@ -3,7 +3,7 @@
 <div class="container py-4">
     <div class="row">
     	  <div class="col-sm-12 col-md-12 col-xl-12 text-center mb-2">
-            <h2 class="h1-responsive font-weight-bold text-center my-4">Why ADLAW</h2>          
+            <h2 class="h1-responsive font-weight-bold text-center my-4">WHY ADLAW</h2>          
         </div>
     <div class="col-sm-12 col-lg-12 col-xs-12 ">
             <p class=" text-justify">
